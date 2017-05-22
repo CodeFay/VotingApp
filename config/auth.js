7b87902e75e21d98c9b8e4d04ceb09ee16a20b9c
CodeFay <luciafey@gmail.com>
2017-05-22 18:01:38 -0500

    set up user authentication (Local and twitter)

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'twitterAuth' : {
+        'consumerKey'       : 'L9xxmnefVy7ESOjDMOD3w2Jgk',
+        'consumerSecret'    : 'p60RfaTm1xTl0NnnKUg0kQ15zMG3V6NVrkPORqMdTIXm5fei3H',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    }
+
+};
